e3d942fffe4ca43041841dc7a33e62ec31bd6745
Gavin Garzia <ggarzia@mines.edu>
2024-09-09 18:57:52 -0600

    project page

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ cat templates/header index.html templates/footer > ../index.html
 # posts
 cd posts
 
-for file in ./*; do
+for file in `ls | sort -k9`; do
     # get title of file and create blog post html file
-    title=${file:2:-5}
+    title=${file:11:-5}
     cat ../templates/header $file ../templates/footer > ../../$title.html
 
     # create card in blog.html
@@ -23,7 +23,14 @@ for file in ./*; do
     echo "$pretty_date <a href=\"./$file\">$pretty_title</a>" >> ../../blog.tmp
 done
 
+cd ../projects
+
+for file in `ls | sort -k9`; do
+    cat $file >> ../../projects.tmp
+done
+
 cd ../../
 cat ./src/templates/header blog.tmp ./src/templates/footer > blog.html
+cat ./src/templates/header projects.tmp ./src/templates/footer > projects.html
 
-rm blog.tmp
+rm *.tmp

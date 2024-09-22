5641dcca253544ebfddb7a8e5a3d56645394f471
Gavin Garzia <ggarzia@mines.edu>
2024-09-22 12:55:20 -0600

    prettier project page

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cat templates/header index.html templates/footer > ../index.html
 
 # posts
 cd posts
-
+echo "building posts"
 for file in `ls | sort -k9`; do
     # get title of file and create blog post html file
     title=${file:11:-5}
@@ -18,14 +18,34 @@ for file in `ls | sort -k9`; do
 
     # create card in blog.html
     ugly_date=$(stat -c '%w' $file)
-    pretty_title=$(echo $title | sed 's/-/ /')
+    pretty_title=$(echo $title | sed 's/-/ /g')
     pretty_date=${ugly_date:0:19}
-    echo "$pretty_date <a href=\"./$file\">$pretty_title</a>" >> ../../blog.tmp
+    : '
+    blog-card template params
+    :TITLE:
+    :DATE:
+    :FILE:
+    :DESC:
+    '
+
+    description=`cat $file | grep DESC | cut -d "|" -f 2`
+    echo "<div class=\"border\">" >> ../../blog.tmp
+    cat ../templates/blog-card | sed\
+        "s/:TITLE:/$pretty_title/g;\
+        s/:FILE:/$title.html/g;\
+        s/:DATE:/$pretty_date/g;\
+        s/:DESC:/$description/g"\
+        >> ../../blog.tmp
+    echo "</div>" >> ../../blog.tmp
 done
 
+#projects
 cd ../projects
+echo "building projects"        
 for file in `ls | sort -k9  -r`; do
+    echo "<div class=\"border\">" >> ../../projects.tmp
     cat $file >> ../../projects.tmp
+    echo "</div>" >> ../../projects.tmp
 done
 
 cd ../../
